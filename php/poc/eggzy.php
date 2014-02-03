182b287d9c8b276f1119b3521900860c44618eb4
Ravi <ravi.gaikwad@imagnate.com>
2014-02-03 18:54:17 +0530

    Added css

--- php/poc/eggzy.php
+++ php/poc/eggzy.php
@@ -1,16 +1,22 @@
 <html>
-<head>
- <script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/createjs.min.js"></script>
-<script type="text/javascript" src="js/Collision.js"></script>
+    <head>
+        <link rel="stylesheet" type="text/css"  href="css/eggzy.css" />
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/createjs.min.js"></script>
+        <script type="text/javascript" src="js/Collision.js"></script>
 
-<script type="text/javascript" src="js/eggzy.js"></script>
-</head>        
-<body >
-        <div class="canvasContainer">
-            <canvas id="mycanvas" width="600" height="1000" style="background:#CFB145; border:1px solid;"></canvas>
-            
-       </div>
-       
+        <script type="text/javascript" src="js/eggzy.js"></script>
+    </head>        
+    <body>
+        <div class="eggzy-game-wrapper">
+            <div class="eggzy-game-headr"></div>
+            <div class="eggzy-canvas-wrapper">
+                <div class="left-sidebar-bg"> </div>
+                <div class="eggzy-game-canvas">
+                    <canvas id="mycanvas" width="720" height="1000" class="canvas-bg" style=""></canvas>
+                </div>
+                <div class="right-sidebar-bg"> </div>
+            </div>
+        </div>
     </body>
 </html>
